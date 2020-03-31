9faf84aebb64b3071e983b64f0533bbd57606c49
Rafael Costa Palma <palma3482@gmail.com>
2020-03-31 19:03:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+{
+    "explorer.confirmDragAndDrop": false,
+    "workbench.colorTheme": "Dracula",
+    "workbench.editor.labelFormat": "short",
+    "workbench.iconTheme": "material-icon-theme",
+    "explorer.compactFolders": false,
+    "javascript.updateImportsOnFileMove.enabled": "never",
+    "typescript.updateImportsOnFileMove.enabled": "never",
+    "breadcrumbs.enabled": true,
+    "editor.parameterHints.enabled": false
+}
